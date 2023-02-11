f3cec6a6b8f9c5db01065213342d07a8eef77c96
Ahmadlaeeq <laeeq343@gmail.com>
2023-02-11 00:54:56 -0800

    summary

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ function App() {
   return (
     <body className='body'>
 <div className='container'>
-<div className='name'><h1 className='title'>Ahmad:Easy Quiz App</h1></div>
+<div className='name'><h1 className='title'>Easy Quiz App</h1></div>
 <div className='queblock'>
-<ol>{<div>{questions.map(q => {return (<div className='statment'><li>{q.statment}</li>
+<ol>{<div>{questions.map(q => {return (<div className='statment'><li>{q.statment}</li><hr className='hr'/>
 <ul>{q.options.map(op => <li><button className='opbtn'>{op}</button></li>)}
 </ul>
 </div>
